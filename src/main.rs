460d9a8736bac809a96d99ae5a52ddcdbcac1e03
Fitti <dev@fitti.io>
2023-01-18 06:25:49 +0100

    Update message handling flow

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use eventsub_websocket::types::TwitchMessage;
-use eventsub_websocket::{event_handler, get_session, parse_message};
+use eventsub_websocket::{event_handler, get_session};
 use std::sync::mpsc;
 use std::thread;
 
 fn main() -> Result<(), String> {
     let (tx, rx) = mpsc::channel();
-    let mut session = get_session()?;
+    let mut session = get_session(None)?;
     let _ = thread::Builder::new()
         .name("handler".into())
         .spawn(move || -> Result<(), String> {
@@ -13,8 +13,7 @@ fn main() -> Result<(), String> {
             Ok(())
         });
     loop {
-        let msg = rx.recv().map_err(|err| format!("{}", err))?;
-        let msg: TwitchMessage = parse_message(&msg).map_err(|err| format!("{}", err))?;
+        let msg: TwitchMessage = rx.recv().map_err(|err| format!("{}", err))?;
         println!("Handling message locally: {:#?}", msg);
     }
 }
