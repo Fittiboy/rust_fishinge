ba1b8f5be35b83fe67119def90bb77ae52fda950
Fitti <dev@fitti.io>
2023-01-27 20:35:37 +0100

    Use macros for DRY code

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
 use fishinge::{create_subscription, get_ids, write_output, Config};
+use fishinge::{if_err_writer, let_match_writer, write_expect};
 
 struct FishingeSetup {
     config: Config,
@@ -92,14 +93,7 @@ fn main() -> Result<()> {
     let output_write3 = Arc::clone(&output);
     let output_read = Arc::clone(&output);
 
-    let event_res = match event_handler(None, tx.clone()) {
-        Ok(event_res) => event_res,
-        Err(err) => {
-            write_output(&output_write1, &err.to_string())
-                .expect("should be able to write to window at this point");
-            return Err(err.into());
-        }
-    };
+    let_match_writer!(event_res, event_handler(None, tx.clone()), output_write1);
 
     let config = Config::load()
         .expect("config has to exist at this point, unless some system operation failed");
@@ -107,70 +101,41 @@ fn main() -> Result<()> {
 
     let notification_handle = thread::Builder::new().name("notifications".into()).spawn(
         move || -> Result<(), anyhow::Error> {
-            if let Err(err) = config.test() {
-                write_output(&output_write2, &err.to_string())
-                    .expect("should be able to write to window at this point");
-                drop(fish_rx);
-                return Err(err);
+            if_err_writer!(config.test(), output_write2, drop(fish_rx));
+            if_err_writer!(fish_rx.recv(), output_write2,);
+            loop {
+                if_err_writer!(fish_rx.recv(), output_write2,);
+                if_err_writer!(handle_notification(&output_write2, &config), output_write2,);
             }
-            if let Err(err) = fish_rx.recv() {
-                write_output(&output_write2, &err.to_string())
-                    .expect("should be able to write to window at this point");
-                return Err(err.into());
-            }
-            let err: anyhow::Error = loop {
-                if let Err(err) = fish_rx.recv() {
-                    break err.into();
-                }
-                if let Err(err) = handle_notification(&output_write2, &config) {
-                    break err;
-                }
-            };
-            write_output(&output_write2, &err.to_string())
-                .expect("should be able to write to window at this point");
-            Err(err)
         },
     );
 
     let listener_handle = thread::Builder::new().name("listener".into()).spawn(
         move || -> Result<(), anyhow::Error> {
             let mut welcome_count = 0;
-            if let Err(err) = fish_tx.send("Healthy!") {
-                write_output(&output_write3, &err.to_string())
-                    .expect("should be able to write to window at this point");
-                return Err(err.into());
-            }
-            let err: anyhow::Error = loop {
-                let msg: TwitchMessage = match rx.recv() {
-                    Ok(msg) => msg,
-                    Err(err) => break err.into(),
-                };
+            if_err_writer!(fish_tx.send("Healthy!"), output_write3,);
+            loop {
+                let_match_writer!(msg, rx.recv(), output_write3);
                 match msg {
                     TwitchMessage::Notification(_) => {
-                        if let Err(err) = fish_tx.send("Ping!") {
-                            break err.into();
-                        }
+                        if_err_writer!(fish_tx.send("Ping!"), output_write3,);
                     }
                     TwitchMessage::Welcome(msg) => {
                         welcome_count += 1;
                         if welcome_count == 1 {
                             let session_id = msg.payload.session.id.to_owned();
-                            if let Err(err) = subscribe(&output_write3, session_id, &config2) {
-                                break err;
-                            }
-                            write_output(&output_write3, "Subscribed!")
-                                .expect("should be able to write to window at this point");
+                            if_err_writer!(
+                                subscribe(&output_write3, session_id, &config2),
+                                output_write3,
+                            );
+                            write_expect!(&output_write3, "Subscribed!");
                         } else {
-                            write_output(&output_write3, "Reconnected to Twitch!")
-                                .expect("should be able to write to window at this point");
+                            write_expect!(&output_write3, "Reconnected to Twitch!");
                         }
                     }
                     _ => {}
                 }
-            };
-            write_output(&output_write3, &err.to_string())
-                .expect("should be able to write to window at this point");
-            Err(err)
+            }
         },
     );
 
@@ -205,21 +170,13 @@ fn handle_notification(output: &Arc<Mutex<String>>, config: &Config) -> Result<(
 }
 
 fn subscribe(output: &Arc<Mutex<String>>, session_id: String, config: &Config) -> Result<()> {
-    let (broadcaster_id, reward_id) = match get_ids(config) {
-        Ok(values) => values,
-        Err(err) => {
-            write_output(output, &err.to_string())
-                .expect("should be able to write to window at this point");
-            return Err(err.into());
-        }
-    };
-    write_output(
+    let_match_writer!((broadcaster_id, reward_id), get_ids(config), *output);
+    write_expect!(
         output,
         &format!(
             "Got ids:\n\tBroadcaster: {},\n\tReward: {}",
             broadcaster_id, reward_id
-        ),
-    )
-    .expect("should be able to write to window at this point");
+        )
+    );
     create_subscription(config, session_id, broadcaster_id, reward_id)
 }
